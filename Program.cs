76f89d69d722d7c3fc52ec9cd344c2661d4cd32e
mitchellm <developer@mitchellmudd.dev>
2023-09-16 18:49:54 -0400

    Removing small project folders

--- Program.cs
+++ Program.cs
@@ -1,22 +1,6 @@
-﻿/*
-    Reverses the provided message, prints out the
-    reversed message and then counts how many times
-    the letter 'o' appears in the message 
-*/
-string str = "The quick brown fox jumps over the lazy dog.";
-char[] charMessage = str.ToCharArray();
+﻿Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
 
-Array.Reverse(charMessage);
-
-int x = 0;
-foreach (char i in charMessage)
-{
-    if (i == 'o')
-    {
-        x++;
-    }
-}
-
-string new_message = new String(charMessage);
-Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {x} times.");
+string myValue = "a";
+Console.WriteLine(myValue == "a");
