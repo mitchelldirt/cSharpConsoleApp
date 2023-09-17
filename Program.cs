cc0fab261d6988632251a9c4478b1366b2ad7950
mitchellm <developer@mitchellmudd.dev>
2023-09-17 11:17:34 -0400

    Completed part 1 of unit 3

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
-﻿Console.WriteLine("a" == "a");
-Console.WriteLine("a" == "A");
-Console.WriteLine(1 == 2);
+﻿// string pangram = "The lazy brown fox jumps over quick dog";
+// Console.WriteLine(pangram.Contains("fox"));
+// Console.WriteLine(pangram.Contains("cow"));
 
-string myValue = "a";
-Console.WriteLine(myValue == "a");
+// int saleAmount = 1000;
+// int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {discount}");
+
+// COIN FLIP CHALLENGE
+// var coinFlipper = new Random();
+// string result = coinFlipper.Next(0, 2) == 0 ? "Heads" : "Tails";
+// Console.WriteLine($"Coin flipping... {result}!");
+
+
+string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin") && level >= 55)
+    Console.WriteLine("Greetings Super Admin!");
+else if (permission.Contains("Admin") && level < 55)
+    Console.WriteLine("Welcome, Admin User");
+else if (permission.Contains("Manager") && level >= 20)
+    Console.WriteLine("Contact an admin for permission");
+else
+    Console.WriteLine("You do not have sufficient privileges.");
