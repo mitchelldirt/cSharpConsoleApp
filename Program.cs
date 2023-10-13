97dcefb5aa584dae103415a3e133d4ca9cea02bc
mitchellm <developer@mitchellmudd.dev>
2023-10-12 21:57:55 -0400

    Almost done!

--- Program.cs
+++ Program.cs
@@ -241,55 +241,179 @@ to update the value. The code then prints the updated value to the console.
 //     }
 // }
 // inputValues is used to store numeric values entered by a user
+//
+// checked
+// {
+//     try
+//     {
+//         int num1 = int.MaxValue;
+//         int num2 = int.MaxValue;
+//         int result = num1 + num2;
+//         Console.WriteLine("Result: " + result);
+//     }
+//     catch (OverflowException ex)
+//     {
+//         Console.WriteLine("Error: The number is too large to be represented as an integer." + ex.Message);
+//     }
+// }
+//
+// try
+// {
+//     string str = null;
+//     int length = str.Length;
+//     Console.WriteLine("String Length: " + length);
+// }
+// catch (NullReferenceException ex)
+// {
+//     Console.WriteLine("Error: The reference is null." + ex.Message);
+// }
+//
+// try
+// {
+//     int[] numbers = new int[5];
+//     numbers[5] = 10;
+//     Console.WriteLine("Number at index 5: " + numbers[5]);
+// }
+// catch (IndexOutOfRangeException ex)
+// {
+//     Console.WriteLine("Error: Index out of range." + ex.Message);
+// }
+//
+// try
+// {
+//     int num3 = 10;
+//     int num4 = 0;
+//     int result2 = num3 / num4;
+//     Console.WriteLine("Result: " + result2);
+// }
+// catch (DivideByZeroException ex)
+// {
+//     Console.WriteLine("Error: Cannot divide by zero." + ex.Message);
+// }
+//
+//
+// Console.WriteLine("Exiting program.");
 
-checked
+// Prompt the user for the lower and upper bounds
+// Console.Write("Enter the lower bound: ");
+// int lowerBound = int.Parse(Console.ReadLine());
+//
+// Console.Write("Enter the upper bound: ");
+// int upperBound = int.Parse(Console.ReadLine());
+//
+// decimal averageValue = 0;
+// bool exit = false;
+//
+// do
+// {
+//     try
+//     {
+// // Calculate the sum of the even numbers between the bounds
+//         averageValue = AverageOfEvenNumbers(lowerBound, upperBound);
+//
+// // Display the value returned by AverageOfEvenNumbers in the console
+//         Console.WriteLine($"The average of even numbers between {lowerBound} and {upperBound} is {averageValue}.");
+//
+//         exit = true;
+//     }
+//     catch (Exception e)
+//     {
+//         Console.WriteLine("An error has occured");
+//         Console.WriteLine(e.Message);
+//         Console.Write($"Enter a new upper bound (or enter Exit to quit): ");
+//         string? userResponse = Console.ReadLine();
+//         if (userResponse.ToLower().Contains("exit"))
+//         {
+//             exit = true;
+//         }
+//         else
+//         {
+//             exit = false;
+//             upperBound = int.Parse(userResponse);
+//         }
+//     }
+// } while (exit == false);
+//
+// // Wait for user input
+// Console.ReadLine();
+//
+// static decimal AverageOfEvenNumbers(int lowerBound, int upperBound)
+// {
+//     if (lowerBound >= upperBound)
+//     {
+//         throw new ArgumentOutOfRangeException("upperBound", "ArgumentOutOfRangeException: upper bound must be greater than lower bound.");
+//     }
+//     
+//     int sum = 0;
+//     int count = 0;
+//     decimal average = 0;
+//
+//     for (int i = lowerBound; i <= upperBound; i++)
+//     {
+//         if (i % 2 == 0)
+//         {
+//             sum += i;
+//             count++;
+//         }
+//     }
+//
+//     average = (decimal)sum / count;
+//
+//     return average;
+// }
+
+string[][] userEnteredValues = new string[][]
 {
-    try
+            new string[] { "1", "2", "3"},
+            new string[] { "1", "two", "3"},
+            new string[] { "0", "1", "2"}
+};
+
+Workflow1(userEnteredValues);
+
+static void Workflow1(string[][] userEnteredValues)
+{
+    foreach (string[] userEntries in userEnteredValues)
     {
-        int num1 = int.MaxValue;
-        int num2 = int.MaxValue;
-        int result = num1 + num2;
-        Console.WriteLine("Result: " + result);
+        try
+        {
+            Process1(userEntries);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("An error occurred during 'Workflow1'.");
+            Console.WriteLine(e.Message);
+        }
     }
-    catch (OverflowException ex)
+}
+
+static void Process1(String[] userEntries)
+{
+    int valueEntered;
+
+    foreach (string userValue in userEntries)
     {
-        Console.WriteLine("Error: The number is too large to be represented as an integer." + ex.Message);
+        bool integerFormat = int.TryParse(userValue, out valueEntered);
+
+        if (integerFormat == true)
+        {
+            if (valueEntered != 0)
+            {
+                checked
+                {
+                    int calculatedValue = 4 / valueEntered;
+                }
+            }
+            else
+            {
+                throw new DivideByZeroException("Invalid data. User input values must be non-zero values.\n");
+            }
+        }
+        else
+        {
+            throw new ArgumentException("Invalid data. User input values must be valid integers.", nameof(userEntries));
+        }
     }
+    
+    Console.WriteLine("'process 1' completed successfully!\n");
 }
-
-try
-{
-    string str = null;
-    int length = str.Length;
-    Console.WriteLine("String Length: " + length);
-}
-catch (NullReferenceException ex)
-{
-    Console.WriteLine("Error: The reference is null." + ex.Message);
-}
-
-try
-{
-    int[] numbers = new int[5];
-    numbers[5] = 10;
-    Console.WriteLine("Number at index 5: " + numbers[5]);
-}
-catch (IndexOutOfRangeException ex)
-{
-    Console.WriteLine("Error: Index out of range." + ex.Message);
-}
-
-try
-{
-    int num3 = 10;
-    int num4 = 0;
-    int result2 = num3 / num4;
-    Console.WriteLine("Result: " + result2);
-}
-catch (DivideByZeroException ex)
-{
-    Console.WriteLine("Error: Cannot divide by zero." + ex.Message);
-}
-
-
-Console.WriteLine("Exiting program.");
